56bfb1c85c7404af240832995a5315016f413260
Ahmed Abdelfattah <ahmed.m.abdelftah@gmail.com>
2018-02-07 16:53:55 +0200

    add content in portfolio section

--- js/my_libs/custom.js
+++ js/my_libs/custom.js
@@ -6,26 +6,30 @@ setTimeout(function () {
         var typewriter = new Typewriter(app, {
             loop: false, typingSpeed: 50, deleteSpeed: 25
         });
-        typewriter.typeString('Do You Need Website?')
+        
+        if(typewriter.typeString('Do You Need Website?')
             .pauseFor(2000)
             .deleteChars(8)
             .typeString('Mobile App?')
             .pauseFor(2000)
             .deleteAll()
             .typeString('Here We Are ActiveDev')
-            .start();
-        setTimeout(function () {
-            $("#siteIntro .typewriter-cursor").hide();
-            var app = document.getElementById('siteDescription');
-            var typewriter = new Typewriter(app, {
-                loop: false, typingSpeed: 60,
-            });
-            typewriter.typeString('We Presents Creative Web & mobile Solutions.')
-                .pauseFor(2500)
-                .start();
-            setTimeout(function () {
-                $(".typewriter-cursor").hide();
-            }, 2500);
-        }, 8900);
+            .start()){
+        
+                setTimeout(function () {
+                    $("#siteIntro .typewriter-cursor").hide();
+                    var app = document.getElementById('siteDescription');
+                    var typewriter = new Typewriter(app, {
+                        loop: false, typingSpeed: 60,
+                    });
+                    typewriter.typeString('We Presents Creative Web & mobile Solutions.')
+                        .pauseFor(2500)
+                        .start();
+                    setTimeout(function () {
+                        $(".typewriter-cursor").hide();
+                    }, 2500);
+                }, 8900);
+                
+            }
     });
 }, 3000);
